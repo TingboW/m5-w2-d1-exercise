039230075ef2d9d1633993869fca35d4ea73ae79
TingboW <104877939+TingboW@users.noreply.github.com>
2022-09-04 17:22:02 -0700

    in class demo

--- demo2.js
+++ demo2.js
@@ -1,7 +1,11 @@
+const { Input } = require("reactstrap");
+
 class Reservation extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        isGoing: true,
+        numberOfGuests: 2
         
       };
   
@@ -9,13 +13,34 @@ class Reservation extends React.Component {
     }
   
     handleInputChange(event) {
-      
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+      this.setState({
+        [name]: value
+      });
     }
   
     render() {
       return (
         <form>
-          
+          <label>
+            Is going:
+            <input 
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input 
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests} 
+            onChange={this.handleInputChange} />
+          </label>
         </form>
       );
     }
